45bdb729a1fb258d4fb62da811601368e4e82a3e
lance <lh160@zcsmart.com>
2019-04-26 14:10:20 +0800

    块同步提取共用配置

--- src/main/scala/org/csc/vrfblk/action/PSBlockSync.scala
+++ src/main/scala/org/csc/vrfblk/action/PSBlockSync.scala
@@ -30,37 +30,38 @@ class PSBlockSync extends PSMVRFNet[PSSyncBlocks] {
   override def service = PSBlockSyncService
 }
 
-//
-// http://localhost:8000/fbs/xdn/pbget.do?bd=
+/**
+  * 请求同步块
+  */
 object PSBlockSyncService extends LogHelper with PBUtils with LService[PSSyncBlocks] with PMNodeHelper {
   override def onPBPacket(pack: FramePacket, pbo: PSSyncBlocks, handler: CompleteHandler) = {
-    //    log.debug("BlockSyncService:" + pack.getFrom())
-    var ret = PRetSyncBlocks.newBuilder();
+    val ret = PRetSyncBlocks.newBuilder();
     if (!VCtrl.isReady()) {
       ret.setRetCode(-1).setRetMessage("VRF Network Not READY")
       handler.onFinished(PacketHelper.toPBReturn(pack, ret.build()))
     } else {
       try {
-        val startTime = System.currentTimeMillis();
         MDCSetBCUID(VCtrl.network())
         MDCSetMessageID(pbo.getMessageId)
         ret.setMessageId(pbo.getMessageId);
-        //
-        val cn = VCtrl.curVN()
         ret.setRetCode(0).setRetMessage("SUCCESS")
-        var totalSize = 0        
+        //同步块总大小(K)
+        var totalSize = 0
+        //同步block总共40M
+        val maxTotalSize = 40 * 1024 * 1024
+
         for (
-          id <- pbo.getStartId to pbo.getEndId if (totalSize <= (40 * 1024 * 1024))
+          id <- pbo.getStartId to pbo.getEndId if totalSize <= maxTotalSize
         ) {
-          val b = VCtrl.loadFromBlock(id, pbo.getNeedBody);          
+          val b = VCtrl.loadFromBlock(id, pbo.getNeedBody)
           if (b != null) {
               b.map(bs => {
-                if (bs !=null && totalSize <= (40 * 1024 * 1024)) {
+                if (bs !=null && totalSize <= maxTotalSize) {
                   totalSize = totalSize + bs.build().toByteArray().size;
-                  if (totalSize <= (40 * 1024 * 1024)) {
+                  if (totalSize <= maxTotalSize) {
                     ret.addBlockHeaders(bs);
                   } else {
-                    log.info("package too large. size=" + totalSize + " blk=" + id);
+                    log.info("package too large. size=" + totalSize + " blk=" + id)
                   } 
                 }
               })
@@ -72,7 +73,7 @@ object PSBlockSyncService extends LogHelper with PBUtils with LService[PSSyncBlo
             b.map(bs => {
               totalSize = totalSize + bs.build().toByteArray().size;
             })
-            if (totalSize <= (40 * 1024 * 1024)) {
+            if (totalSize <= maxTotalSize) {
               b.map(bs => {
                 if (bs !=null) {
                   ret.addBlockHeaders(bs);
