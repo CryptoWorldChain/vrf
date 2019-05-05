bb8b012569100594e064a038cb2dcb161d2578f6
brew <brew@cwv.io>
2019-05-05 15:37:11 +0800

    o

--- src/main/scala/org/csc/vrfblk/action/PSCoinbaseNew.scala
+++ src/main/scala/org/csc/vrfblk/action/PSCoinbaseNew.scala
@@ -29,6 +29,7 @@ import org.csc.vrfblk.Daos
 import org.csc.vrfblk.tasks.BeaconGossip
 import org.apache.commons.lang3.StringUtils
 import org.csc.ckrand.pbgens.Ckrand.VNodeState
+import org.csc.vrfblk.utils.VConfig
 
 @NActorProvider
 @Instantiate
@@ -56,7 +57,8 @@ object PSCoinbaseNewService extends LogHelper with PBUtils with LService[PSCoinb
       if (parentBlock == null) {
         log.warn("not found parent block:: bh=" + Daos.enc.hexEnc(block.getHeader.getHash.toByteArray()) + " height=" + block.getHeader.getNumber)
         if (VCtrl.curVN().getState != VNodeState.VN_INIT
-          && VCtrl.curVN().getState != VNodeState.VN_SYNC_BLOCK) {
+          && VCtrl.curVN().getState != VNodeState.VN_SYNC_BLOCK
+          && VCtrl.curVN().getCurBlock + VConfig.MAX_SYNC_BLOCKS > pbo.getBlockHeight ) {
           BlockProcessor.offerBlock(new ApplyBlock(pbo)); //need to sync or gossip
         }else{
           
@@ -66,14 +68,14 @@ object PSCoinbaseNewService extends LogHelper with PBUtils with LService[PSCoinb
         val nodebits = parentBlock.getMiner.getBit;
         val (hash, sign) = RandFunction.genRandHash(Daos.enc.hexEnc(block.getHeader.getPreHash.toByteArray()), parentBlock.getMiner.getTermid, nodebits);
         if (hash.equals(block.getMiner.getTermid) || block.getHeader.getNumber == 1) {
-          BlockProcessor.offerMessage(new ApplyBlock(pbo));
+          BlockProcessor.offerBlock(new ApplyBlock(pbo));
         } else {
           //if rollback
           if (StringUtils.isNotBlank(BeaconGossip.rollbackGossipNetBits)) {
             val (rollbackhash, rollblacksign) = RandFunction.genRandHash(Daos.enc.hexEnc(parentBlock.getHeader.getHash.toByteArray()), parentBlock.getMiner.getTermid, BeaconGossip.rollbackGossipNetBits);
             if (rollbackhash.equals(block.getMiner.getTermid)) {
               log.info("rollback hash apply:rollbackhash=" + rollbackhash + ",blockheight=" + pbo.getBlockHeight);
-              BlockProcessor.offerMessage(new ApplyBlock(pbo));
+              BlockProcessor.offerBlock(new ApplyBlock(pbo));
             } else {
               log.warn("beaconhash.rollback not equal:height=" + block.getHeader.getNumber + ":: BH=" + pbo.getBlockEntry.getBlockhash
                 + " prvbh=" + Daos.enc.hexEnc(block.getHeader.getPreHash.toByteArray()) + " dbprevbh=" + Daos.enc.hexEnc(parentBlock.getHeader.getHash.toByteArray())
